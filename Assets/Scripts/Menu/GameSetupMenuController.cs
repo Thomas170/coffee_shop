5265e242287311bcb48ab273d7b959fadba8a159
Thomas Cacelles <thomascacelles@icloud.com>
2025-05-20 15:37:10 +0200

    menu

--- Assets/Scripts/Menu/GameSetupMenuController.cs
+++ Assets/Scripts/Menu/GameSetupMenuController.cs
@@ -1,7 +1,63 @@
+using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class GameSetupMenuController : BaseMenuController
 {
+    [Header("Top Right Info")]
+    [SerializeField] private TextMeshProUGUI levelText;
+    [SerializeField] private TextMeshProUGUI coinsText;
+
+    [Header("Player Slots")]
+    [SerializeField] private GameObject[] playerSlots;
+    
+    [Header("Copy Code")]
+    [SerializeField] private TextMeshProUGUI codeToCopy;
+
+    private readonly int _level = 3;
+    private readonly int _coins = 150;
+
+    private void OnEnable()
+    {
+        UpdateTopRightInfo();
+        UpdatePlayerSlots();
+    }
+
+    private void UpdateTopRightInfo()
+    {
+        levelText.text = $"Niveau {_level}";
+        coinsText.text = $"{_coins} pièces";
+    }
+
+    private void UpdatePlayerSlots()
+    {
+        for (int i = 0; i < playerSlots.Length; i++)
+        {
+            TextMeshProUGUI pseudoText = playerSlots[i].transform.Find("Pseudo").GetComponent<TextMeshProUGUI>();
+            GameObject inviteObject = playerSlots[i].transform.Find("Invite").gameObject;
+            if (i == 0)
+            {
+                pseudoText.text = "Player";
+                pseudoText.gameObject.SetActive(true);
+                inviteObject.SetActive(false);
+            }
+            else
+            {
+                pseudoText.gameObject.SetActive(false);
+                inviteObject.SetActive(true);
+            }
+        }
+    }
+    
+    public void CopyCodeToClipboard()
+    {
+        if (codeToCopy != null)
+        {
+            GUIUtility.systemCopyBuffer = codeToCopy.text;
+            Debug.Log($"Copied to clipboard: {codeToCopy.text}");
+        }
+    }
+
     public override void ExecuteMenuAction(string buttonName)
     {
         switch (buttonName)
--- Assets/Scripts/Menu/JoinMenuController.cs
+++ Assets/Scripts/Menu/JoinMenuController.cs
@@ -1,18 +1,49 @@
+using UnityEngine;
+using TMPro;
+
 public class JoinMenuController : BaseMenuController
 {
+    [Header("Join Settings")] [SerializeField]
+    private TMP_InputField codeInputField;
+
+    [SerializeField] private GameObject errorMessageObject;
+
     public GameSetupMenuController gameSetupMenuController;
-    
+
     public override void ExecuteMenuAction(string buttonName)
     {
         switch (buttonName)
         {
             case "Join":
-                CloseMenu();
-                gameSetupMenuController.OpenMenu();
+                TryJoin();
                 break;
             case "Back":
                 HandleBack();
                 break;
         }
+    }
+
+    private void TryJoin()
+    {
+        string code = codeInputField.text.Trim();
+
+        if (string.IsNullOrEmpty(code))
+        {
+            errorMessageObject.SetActive(true);
+        }
+        else
+        {
+            errorMessageObject.SetActive(false);
+            CloseMenu();
+            gameSetupMenuController.OpenMenu();
+        }
+    }
+
+    public override void OpenMenu()
+    {
+        codeInputField.text = "";
+        errorMessageObject.SetActive(false);
+        
+        base.OpenMenu();
     }
 }
